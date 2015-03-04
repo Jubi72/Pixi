cd7557ebce476914350be45ec065510b45884c62
J. B. <jubi721@gmail.com>
2015-03-04 15:00:25 +0100

    Update pixi.py
    Grundgerüst erstellt

--- pixi.py
+++ pixi.py
@@ -1,1 +1,19 @@
 # Funktionen
+# Pixi - Graphical Portable Pixelmap Editor
+# Created by Julius Bittner 04.03.2015
+# Last change 04.03.2015
+
+class Pixi:
+
+    def __init__(self):
+        # initialisiert Editor
+        self.einliste = []
+        self.kopf = []
+        self.masse = 0
+        self.breite = 0
+        self.hoehe = 0
+        self.farbtiefe = 0
+        self.punktliste = []
+
+    def binary (self):
+        pass
