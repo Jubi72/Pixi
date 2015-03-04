b48f52b2c7fc95c18835c284aec3909b3073406a
J. B. <jubi721@gmail.com>
2015-03-04 15:01:16 +0100

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,14 @@
+# Graphical User Interface
+# Pixi - Graphical Portable Pixelmap Editor
+# Created by Julius Bittner 04.03.2015
+# Last change 04.03.2015
+
+import pixi
+import tkinter as tk
+
+class Gui:
+
+    def __init__(self):
+        pass
+        #self.root = tk.Tk()
+        
